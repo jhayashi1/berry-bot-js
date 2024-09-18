f661314d380849052bba2efc729e4500e4a68eb6
jhayashi1 <jaredhayashi@gmail.com>
2024-09-17 21:43:35 -0500

    feat: basic bot init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import {REST, Routes} from 'discord.js';
+import 'dotenv/config';
+import type {SlashCommandBuilder} from 'discord.js';
+import {Client as DiscordClient, Collection, Events, GatewayIntentBits} from 'discord.js';
+
+interface Client extends DiscordClient {
+    commands?: Collection<string, SlashCommandBuilder>;
+}
 
 const token = process.env.TOKEN;
 
-const rest = new REST({version: '10'}).setToken(token);
+const initClient = (): Client => {
+    const client: Client = new DiscordClient({intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds]});
+    client.commands = new Collection();
+
+    client.once(Events.ClientReady, (readyClient) => {
+        console.log(`logged in as ${readyClient.user.tag}`);
+    });
+
+
+    return client;
+};
+
+const client = initClient();
+
+client.login(token);
